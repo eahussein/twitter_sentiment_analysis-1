c998f559aa1928b4350daa3d55064557cd4f6d2f
hussein <eslam@idia.ac.za>
2022-10-05 22:00:02 +0200

    final changes

--- Tutorial1/twitter_credentials.py
+++ Tutorial1/twitter_credentials.py
@@ -2,8 +2,8 @@
 # Variables which contain the user credentials needed to access the Twitter API
 # Copy and paste your credentials within the quotation marks '' as these are string variables
 
-api_key = 'AOqllaltMdOgeJQ8ziYYiwcSg'
-api_secret_key =  'cyHr46KCUA19kFcn5gT2cZOfiSuKFCW3sVi5PDj4Ciufi7HFov'
-access_token = '1567133935930318848-3XGcpUNtkrUcHXQSx3bUxEBCwTHPTF'
-access_token_secret = 'ftbufVxuHoOshKnfmCxUBfUW0GarnKAZmYJv6BIPpP98d'
+api_key = ''
+api_secret_key =  ''
+access_token = ''
+access_token_secret = ''
 
